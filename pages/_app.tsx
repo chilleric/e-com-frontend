2db77d5a0491e9d25bf432f2d6ee7b05378ed006
ericchentch <ericchen.tch@gmail.com>
2022-10-27 13:09:44 +0700

    update lib

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,13 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-import { CacheProvider } from '@emotion/react';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-
-import createEmotionCache from '@/utils/mui/EmotionCache';
-
 import store from '@/redux/store';
-import { MyLightThemes } from '@/styles/themes/MyThemes';
 import { Provider } from 'react-redux';
 
-const clientSideEmotionCache = createEmotionCache();
-
 function MyApp({ Component, pageProps }: AppProps) {
-  const emotionCache = clientSideEmotionCache;
-
   return (
     <Provider store={store}>
-      <CacheProvider value={emotionCache}>
-        <ThemeProvider theme={MyLightThemes}>
-          <Component {...pageProps} />
-          <CssBaseline />
-        </ThemeProvider>
-      </CacheProvider>
+      <Component {...pageProps} />
     </Provider>
   );
 }
